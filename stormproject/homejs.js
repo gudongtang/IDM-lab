8344cf2a3df0eaf24ab2f5f11dfc10d4e160c301
tangshengkai <812558302@qq.com>
2018-08-21 21:21:50 +0800

    唐圣凯

---
+++ stormproject/homejs.js
@@ -0,0 +1,36 @@
+var data;
+data = {weak:0,pant:0,voice:0,fever:0};
+function Click(number1,number2) {
+  switch(number1)
+  {
+    case 2:
+      data.weak = number2;
+      break;
+    case 3:
+      data.pant = number2;
+      break;
+    case 4:
+      data.voice = number2;
+      break;
+    case 5:
+      data.fever = number2;
+      break;
+  }
+};
+function Sub() {
+  var finish = true;
+  for(var i in data) {
+    if(data[i] == 0)
+    {
+      finish = false;
+    }
+  }
+if(finish)
+{
+  console.log(data);
+}
+else
+{
+  alert("请选择完成全部问题");
+}
+}
