4559b5591ca083b660e2f45a7c5d10a295a098a8
tangshengkai <812558302@qq.com>
2018-08-28 16:08:09 +0800

    唐圣凯

--- stormproject/homejs.js
+++ stormproject/homejs.js
@@ -1,41 +1,37 @@
 var data = {};
 function Click(number1,number2) {
-  data[number1] = number2;
+  var questname = "member" + number1;
+  data[questname] = number2;
 }
 function Sub() {
   var finish = true;
-  if(Object.getOwnPropertyNames(data).length !=4)
+  if(Object.getOwnPropertyNames(data).length !==4)
   {
     finish = false;
   }
-if(finish)
-{
-  var jsondata = function (data) {
-    var objData = {};
-
-    data.forEach((value, key) => objData[key] = value);
-
-    return JSON.stringify(objData);
-  };
-  $.ajax(
-    {
-      async:false,
-      type:"POST",
-      url:"http://127.0.0.1:8087/submit",
-      dataType: "json",
-      contentType:"text/plain;charset=UTF-8",
-      data:JSON.stringify(data),
-      success:function (Data) {
-        alert(Data);
-      },
-      error:function (Data) {
-        console.log(Data);
+  if(finish)
+  {
+    data.name = "唐圣凯";
+    data.IDnumber = "610402156345278649";
+    $.ajax(
+      {
+        async:false,
+        type:"POST",
+        url:"http://127.0.0.1:8087/submit",
+        dataType: "json",
+        contentType:"application/json;charset=UTF-8",
+        data:JSON.stringify(data),
+        success:function (Data) {
+          console.log(Data);
+        },
+        error:function (Data) {
+          console.log(Data);
+        }
       }
-    }
-  );
-}
-else
-{
-  alert("请选择完成全部问题");
-}
+    );
+  }
+  else
+  {
+    alert("请选择完成全部问题");
+  }
 }
